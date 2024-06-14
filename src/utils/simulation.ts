9fbb1b74206ae544fc6d3ee5e8919876b9a5e2d5
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-06-14 01:00:25 -0500

    Add simulatePlay function

--- src/utils/simulation.ts
+++ src/utils/simulation.ts
@@ -1,5 +1,6 @@
 import { Move } from "../strategies/types";
 import { Strategy } from "../strategies/Strategy";
+import { StrategyName, strategyClasses } from "../strategies";
 
 interface RoundResult {
   round: number;
@@ -28,4 +29,64 @@ export const simulateRound = (
   strategy2.setOpponentMove(move1);
 
   return [move1, move2];
+};
+
+export const simulatePlay = (
+  strategy1Name: StrategyName,
+  strategy2Name: StrategyName,
+  rounds: number = 20
+): PlayResult => {
+  const Strategy1 = strategyClasses[strategy1Name];
+  const Strategy2 = strategyClasses[strategy2Name];
+
+  const strategy1 = new Strategy1();
+  const strategy2 = new Strategy2();
+
+  let strategy1TotalPoints = 0;
+  let strategy2TotalPoints = 0;
+  const roundResults: RoundResult[] = [];
+
+  for (let round = 0; round < rounds; round++) {
+    const [move1, move2] = simulateRound(strategy1, strategy2);
+
+    let strategy1Points = 0;
+    let strategy2Points = 0;
+
+    if (move1 && move2) {
+      // Both cooperate
+      strategy1Points = 3;
+      strategy2Points = 3;
+    } else if (move1 && !move2) {
+      // Strategy 1 Cooperates, Strategy 2 Defects
+      strategy1Points = 0;
+      strategy2Points = 5;
+    } else if (!move1 && move2) {
+      // Strategy 1 Defects, Strategy 2 Cooperates
+      strategy1Points = 5;
+      strategy2Points = 0;
+    } else {
+      // Both Defect
+      strategy1Points = 1;
+      strategy2Points = 1;
+    }
+
+    strategy1TotalPoints += strategy1Points;
+    strategy2TotalPoints += strategy2Points;
+
+    roundResults.push({
+      round: round + 1,
+      strategy1Move: move1,
+      strategy2Move: move2,
+      strategy1Points: strategy1Points,
+      strategy2Points: strategy2Points,
+    });
+  }
+
+  return {
+    rounds: roundResults,
+    finalScore: {
+      strategy1: strategy1TotalPoints,
+      strategy2: strategy2TotalPoints,
+    },
+  };
 };
