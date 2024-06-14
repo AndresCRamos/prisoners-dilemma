d6c151fe77391a8610d8ddbb183593154764d119
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-06-14 00:16:42 -0500

    Implement Tit4Tat strategy

---
+++ src/strategies/TitForTat.ts
@@ -0,0 +1,19 @@
+import { Move } from "./types";
+import { Strategy } from "./strategy";
+
+class TitForTat extends Strategy {
+  private lastOpponentMove: Move | null = null;
+
+  getNextMove(): Move {
+    if (this.lastOpponentMove === null) {
+      return true;
+    }
+    return this.lastOpponentMove;
+  }
+
+  setOpponentMove(move: Move): void {
+    this.lastOpponentMove = move;
+  }
+}
+
+export default TitForTat;
