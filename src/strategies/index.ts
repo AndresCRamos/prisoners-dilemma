f67209fadbf98ec5b385eb27b39ebae573109484
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-06-14 01:28:52 -0500

    Add better type support for strategies module

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -1,9 +1,20 @@
+import { Strategy } from "./Strategy";
 import TitForTat from "./TitForTat";
 import RandomStrategy from "./RandomStrategy";
 
-export const strategyClasses = {
-  "Tit for Tat": TitForTat,
-  Random: RandomStrategy,
+export type StrategyClassMap = {
+  [key: string]: new () => Strategy;
 };
 
-export type StrategyName = keyof typeof strategyClasses;
+const strategyClassesConst = {
+  "Tit for Tat": TitForTat,
+  Random: RandomStrategy,
+} as const;
+
+export type StrategyName = keyof typeof strategyClassesConst;
+export const strategyNames: StrategyName[] = Object.keys(
+  strategyClassesConst
+) as StrategyName[];
+
+// Ensuring the strategyClasses object conforms to StrategyClassMap type
+export const strategyClasses: StrategyClassMap = strategyClassesConst;
