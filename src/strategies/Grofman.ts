0d1b173db3147f25e8a1fe1da97bf66b16b6b15d
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-06-14 19:11:04 -0500

    Implement Grofman strategy

---
+++ src/strategies/Grofman.ts
@@ -0,0 +1,26 @@
+import { random_move_with_prob } from "../utils/random";
+import { Strategy } from "./Strategy";
+import { Move } from "./types";
+
+class Grofman extends Strategy {
+  private lastMove: Move | null = null;
+  private lastOpponentMove: Move | null = null;
+
+  getNextMove(): boolean {
+    let nextMove = false;
+    if (this.lastMove === this.lastOpponentMove) {
+      nextMove = true;
+    } else {
+      nextMove = random_move_with_prob(2 / 7);
+    }
+
+    this.lastMove = nextMove;
+    return nextMove;
+  }
+
+  setOpponentMove(move: boolean): void {
+    this.lastOpponentMove = move;
+  }
+}
+
+export default Grofman;
