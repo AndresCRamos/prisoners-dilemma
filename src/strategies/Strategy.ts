cdd488f9167949e42e477bd989bc6eac29ef1563
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-06-26 15:19:01 -0500

    Create Strategy With Rounds abstract class

--- src/strategies/Strategy.ts
+++ src/strategies/Strategy.ts
@@ -4,3 +4,17 @@ export abstract class BaseStrategy implements StrategyInterface {
   abstract getNextMove(): Move;
   abstract setOpponentMove(move: Move): void;
 }
+
+export abstract class StrategyWithRounds extends BaseStrategy {
+  protected rounds: number;
+
+  constructor(rounds: number) {
+    super();
+    this.rounds = rounds;
+  }
+
+  abstract getNextMove(): Move;
+  abstract setOpponentMove(move: Move): void;
+}
+
+export type Strategy = BaseStrategy | StrategyWithRounds;
