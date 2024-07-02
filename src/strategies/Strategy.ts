f45ca02ef29a52c458b02ab93507bb611cee48ec
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-07-02 01:10:07 -0500

    Add StrategyChi2test class

--- src/strategies/Strategy.ts
+++ src/strategies/Strategy.ts
@@ -1,3 +1,4 @@
+import chi2gof from "@stdlib/stats-chi2gof";
 import { Move, StrategyInterface } from "./types";
 
 export abstract class BaseStrategy implements StrategyInterface {
@@ -17,7 +18,41 @@ export abstract class StrategyWithRounds extends BaseStrategy {
   abstract setOpponentMove(move: Move): void;
 }
 
-export type Strategy = BaseStrategy | StrategyWithRounds;
+export class StrategyChi2Test extends BaseStrategy {
+  protected opponentCooperations: number = 0;
+  protected opponentDefections: number = 0;
+  protected alpha: number = 0.05;
+  protected currentRound: number = 0;
+
+  protected addMove(move: Move): void {
+    if (move) {
+      this.opponentCooperations++;
+    } else {
+      this.opponentDefections++;
+    }
+  }
+
+  protected chi2test(): { pValue: number; isRandom: boolean } {
+    const observed = [this.opponentDefections, this.opponentCooperations];
+    const expected = [this.currentRound / 2, this.currentRound / 2];
+
+    const result = chi2gof(observed, expected);
+    const pValue = result.pValue;
+
+    return { pValue, isRandom: pValue >= this.alpha };
+  }
+
+  getNextMove(): Move {
+    throw new Error("getNextMove must be implemented");
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  setOpponentMove(_move: Move): void {
+    throw new Error("setOpponentMove must be implemented");
+  }
+}
+
+export type Strategy = BaseStrategy | StrategyWithRounds | StrategyChi2Test;
 
 export type BaseStrategyConstructor = new () => Strategy;
 export type StrategyWithRoundsConstructor = new (rounds: number) => Strategy;
