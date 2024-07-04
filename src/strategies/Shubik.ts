85c780681e14adac577ade6b078a58bace5b33f0
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-07-03 23:22:17 -0500

    Implement Shubik strategy get next move method

--- src/strategies/Shubik.ts
+++ src/strategies/Shubik.ts
@@ -8,10 +8,33 @@ class Shubik extends StrategyBase {
   private lastOpponentMove: Move | null = null;
   private lastOwnMove: Move | null = null;
 
-  getNextMove(): Move {}
+  private setLastOwnMove(move: Move): Move {
+    this.lastOwnMove = move;
+    return this.lastOwnMove;
+  }
+
+  getNextMove(): Move {
+    if (this.lastOpponentMove === null) {
+      return this.setLastOwnMove(true);
+    } else if (this.isRetaliating) {
+      return this.setLastOwnMove(false);
+    }
+    if (!this.lastOpponentMove && this.lastOwnMove) {
+      this.isRetaliating = true;
+      this.retaliationLenght += 1;
+      this.retaliationRemaining = this.retaliationLenght;
+      return this.setLastOwnMove(false);
+    }
+    return this.setLastOwnMove(true);
+  }
 
   setOpponentMove(move: Move): void {
-    if (this.isRetaliating || (!this.lastOpponentMove && this.lastOwnMove)) {
+    if (
+      this.isRetaliating ||
+      (this.lastOpponentMove !== null &&
+        !this.lastOpponentMove &&
+        this.lastOwnMove)
+    ) {
       this.retaliationRemaining--;
       if (this.retaliationRemaining == 0) {
         this.isRetaliating = false;
