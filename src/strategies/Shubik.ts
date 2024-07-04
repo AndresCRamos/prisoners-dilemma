049262a25578dc48a6db5c8915377799a214797c
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-07-03 21:06:59 -0500

    Add shubik strategy basic class properties

--- src/strategies/Shubik.ts
+++ src/strategies/Shubik.ts
@@ -1,8 +1,16 @@
 import { StrategyBase } from "./Strategy";
+import { Move } from "./types";
 
 class Shubik extends StrategyBase {
-  getNextMove(): boolean {}
-  setOpponentMove(move: boolean): void {}
+  private isRetaliating: boolean = false;
+  private retaliationLenght: number = 0;
+  private retaliationRemaining: number = 0;
+  private lastOpponentMove: Move | null = null;
+  private lastOwnMove: Move | null = null;
+
+  getNextMove(): Move {}
+
+  setOpponentMove(move: Move): void {}
 }
 
 export default Shubik;
