b47a7a316cff21ef61ab27ccbd5f9c02d4e50b6b
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-07-09 17:31:06 -0500

    Implement Graaskamp strategy

--- src/strategies/Graaskamp.ts
+++ src/strategies/Graaskamp.ts
@@ -1,10 +1,70 @@
+import { random_uniform } from "../utils/random";
 import { StrategyChi2TestWithRounds } from "./Strategy";
 import { Move } from "./types";
 
 class Graaskamp extends StrategyChi2TestWithRounds {
-  getNextMove(): Move {}
+  private isOpponentRandom: boolean = false;
+  private ownMoveHistory: Move[] = [];
+  private opponentMoveHistory: Move[] = [];
+  private nextRandomDefectionTurn: number | null = null;
 
-  setOpponentMove(move: Move): void {}
+  private setLastOwnMove(move: Move): Move {
+    this.ownMoveHistory.push(move);
+    return move;
+  }
+
+  getNextMove(): Move {
+    if (this.currentRound === 0) {
+      return this.setLastOwnMove(true);
+    }
+
+    if (this.currentRound < 10) {
+      if (!this.opponentMoveHistory.at(-1) || this.currentRound === 8) {
+        return this.setLastOwnMove(false);
+      }
+      return this.setLastOwnMove(true);
+    }
+    const { isRandom } = this.chi2test();
+    this.isOpponentRandom = this.isOpponentRandom || isRandom;
+
+    if (this.isOpponentRandom) {
+      return this.setLastOwnMove(false);
+    }
+
+    const playsTitForTat = this.ownMoveHistory
+      .slice(1)
+      .every((move, index) => move === this.opponentMoveHistory[index]);
+    const isClone = this.ownMoveHistory.every(
+      (move, index) => move === this.opponentMoveHistory[index]
+    );
+
+    if (playsTitForTat || isClone) {
+      if (!this.opponentMoveHistory.at(-1)) {
+        return this.setLastOwnMove(false);
+      }
+      return this.setLastOwnMove(true);
+    }
+
+    if (this.nextRandomDefectionTurn === null) {
+      this.nextRandomDefectionTurn =
+        Math.round(random_uniform(5, 15)) + this.currentRound;
+    }
+
+    if (this.currentRound === this.nextRandomDefectionTurn) {
+      // Resample the next defection turn
+      this.nextRandomDefectionTurn =
+        Math.round(random_uniform(5, 15)) + this.currentRound;
+      return this.setLastOwnMove(false); // Defect
+    }
+
+    return this.setLastOwnMove(true); // Cooperate
+  }
+
+  setOpponentMove(move: Move): void {
+    this.addMove(move);
+    this.opponentMoveHistory.push(move);
+    this.currentRound++;
+  }
 }
 
 export default Graaskamp;
