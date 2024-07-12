b924c8215f5b7174b6c810bb187f7efb94d9b3cf
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-07-12 10:05:48 -0500

    Implement Tullock strategy getNextMove func

--- src/strategies/Tullock.ts
+++ src/strategies/Tullock.ts
@@ -1,3 +1,4 @@
+import { random_move_with_prob } from "../utils/random";
 import { StrategyBase } from "./Strategy";
 import { Move } from "./types";
 
@@ -6,7 +7,17 @@ class Tullock extends StrategyBase {
   private opponentMoveHistory: Move[] = [];
   private currentRound: number = 0;
 
-  getNextMove(): Move {}
+  getNextMove(): Move {
+    if (this.currentRound < this.roundsToCooperate) {
+      return true;
+    }
+    const rounds = this.roundsToCooperate--;
+    const cooperateCount = this.opponentMoveHistory
+      .slice(-rounds)
+      .filter((move) => move).length;
+    const prob_cooperate = Math.max(0, cooperateCount / rounds - 0.1);
+    return random_move_with_prob(prob_cooperate);
+  }
 
   setOpponentMove(move: Move): void {
     this.opponentMoveHistory.push(move);
