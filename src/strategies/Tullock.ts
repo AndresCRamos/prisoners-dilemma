acf19e5a059a7d7222beffd24fff9952071b652e
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-07-12 09:57:05 -0500

    Add empty Tullokc strategy class

---
+++ src/strategies/Tullock.ts
@@ -0,0 +1,12 @@
+import { StrategyBase } from "./Strategy";
+import { Move } from "./types";
+
+class Tullock extends StrategyBase {
+  private roundsToCooperate: number = 11;
+  private opponentMoveHistory: Move[] = [];
+  private currentRound: number = 0;
+
+  getNextMove(): Move {}
+
+  setOpponentMove(move: Move): void {}
+}
