b8f8268e93b9a6ebc7bce23e4215936a03ade898
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-07-13 22:08:42 -0500

    Add empty Downing strategy class

---
+++ src/strategies/Downing.ts
@@ -0,0 +1,18 @@
+import { StrategyWithHistory } from "./Strategy";
+import { Move } from "./types";
+
+class Downing extends StrategyWithHistory {
+  private opponentCooperationsInResponseToCooperation: number = 0;
+  private opponentCooperationsInResponseToDefection: number = 0;
+  private ownCooperations: number = 0;
+  private ownDefections: number = 0;
+
+  private mutualDefectionPoints = 1;
+  private mutualCooperationPoints = 3;
+  private cooperationOnDefectionPoints = 0;
+  private defectionOnCooperationPoints = 5;
+
+  getNextMove(): Move {}
+
+  setOpponentMove(move: Move): void {}
+}
