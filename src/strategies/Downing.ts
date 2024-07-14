9766a8ad401d98642a937d5aa2dd93442127e0a4
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-07-13 22:47:11 -0500

    Implement Downing strategy getNextMove func

--- src/strategies/Downing.ts
+++ src/strategies/Downing.ts
@@ -12,7 +12,47 @@ class Downing extends StrategyWithHistory {
   private S_COOPERATION_ON_DEFECTION_POINTS = 0;
   private T_DEFECTION_ON_COOPERATION_POINTS = 5;
 
-  getNextMove(): Move {}
+  getNextMove(): Move {
+    if (this.currentRound == 0) {
+      return this.setLastOwnMove(false);
+    }
+
+    if (this.currentRound == 1) {
+      if (this.opponentMoveHistory.at(-1)) {
+        this.opponentCooperationsInResponseToCooperation++;
+      }
+      return this.setLastOwnMove(false);
+    }
+
+    if (this.ownMoveHistory.at(-2) && this.opponentMoveHistory.at(-1)) {
+      this.opponentCooperationsInResponseToCooperation++;
+    }
+
+    if (!this.ownMoveHistory.at(-2) && this.opponentMoveHistory.at(-1)) {
+      this.opponentCooperationsInResponseToDefection++;
+    }
+
+    const alpha =
+      this.opponentCooperationsInResponseToCooperation /
+      (this.ownCooperations + 1);
+    const beta =
+      this.opponentCooperationsInResponseToDefection / this.ownDefections;
+
+    const expected_value_of_cooperating =
+      alpha * this.R_MUTUAL_COOPERATION_POINTS +
+      (1 - alpha) * this.S_COOPERATION_ON_DEFECTION_POINTS;
+    const expected_value_of_defecting =
+      beta * this.T_DEFECTION_ON_COOPERATION_POINTS +
+      (1 - beta) * this.P_MUTUAL_DEFECTION_POINTS;
+
+    if (expected_value_of_cooperating > expected_value_of_defecting) {
+      return this.setLastOwnMove(true);
+    }
+    if (expected_value_of_defecting < expected_value_of_cooperating) {
+      return this.setLastOwnMove(false);
+    }
+    return this.setLastOwnMove(!this.ownMoveHistory.at(-1));
+  }
 
   protected setLastOwnMove(move: Move): Move {
     this.ownMoveHistory.push(move);
