35d8c5fe74a46927c14d3347ce99a868ae425c93
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-07-17 23:34:51 -0500

    Create Nydegger strategy constants

--- src/strategies/Nydegger.ts
+++ src/strategies/Nydegger.ts
@@ -2,6 +2,17 @@ import { StrategyWithHistory } from "./Strategy";
 import { Move } from "./types";
 
 class Nydegger extends StrategyWithHistory {
+  private pointsToDefect: number[] = [
+    1, 6, 7, 17, 22, 23, 26, 29, 30, 31, 33, 38, 39, 45, 49, 54, 55, 58, 61,
+  ];
+
+  private scoreMap = new Map<[boolean, boolean], number>([
+    [[true, true], 0],
+    [[true, false], 2],
+    [[false, true], 1],
+    [[false, false], 3],
+  ]);
+
   getNextMove(): Move {}
 
   setOpponentMove(move: Move): void {
