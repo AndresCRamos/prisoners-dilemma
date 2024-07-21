9717eeee33cf176ef5f6b293858b8856f85432ad
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-07-20 21:31:44 -0500

    Implement TidemanAndChieruzzi strategy helper functions

--- src/strategies/TidemanAndChieruzzi.ts
+++ src/strategies/TidemanAndChieruzzi.ts
@@ -7,17 +7,50 @@ class TidemanAndChieruzzi extends StrategyBase {
   private lastOwnMove: Move | null = null;
   private ownScore: number = 0;
   private isRetaliating: boolean = false;
-  private retaliationLenght: number = 0;
+  private retaliationLength: number = 0;
   private retaliationRemaining: number = 0;
   private lastFreshStart: number = 0;
-  private freshStart: boolean = false;
+  private isFreshStart: boolean = false;
   private rememberedNumberOfOpponentDefectioons: number = 0;
-  private scorePerRoundMap = new Map<[boolean, boolean], number>([
-    [[true, true], 3],
-    [[true, false], 0],
-    [[false, true], 5],
-    [[false, false], 1],
+  private scorePerRoundMap = new Map<[boolean, boolean], [number, number]>([
+    [
+      [true, true],
+      [3, 3],
+    ],
+    [
+      [true, false],
+      [0, 5],
+    ],
+    [
+      [false, true],
+      [5, 0],
+    ],
+    [
+      [false, false],
+      [1, 1],
+    ],
   ]);
+
+  private freshStart() {
+    this.isRetaliating = false;
+    this.retaliationLength = 0;
+    this.retaliationRemaining = 0;
+    this.rememberedNumberOfOpponentDefectioons = 0;
+  }
+
+  private addScoresFromLastRound(): [number, number] {
+    const [lastOwnScore, lastOpponentScore] = this.scorePerRoundMap.get(
+      [this.lastOwnMove!, this.lastOpponentMove!]!
+    )!;
+    this.ownScore += lastOwnScore;
+    this.opponentScore += lastOpponentScore;
+  }
+
+  private setLastOwnMove(move: Move): Move {
+    this.lastOwnMove = move;
+    return move;
+  }
+
   getNextMove(): Move {}
 
   setOpponentMove(move: Move): void {}
