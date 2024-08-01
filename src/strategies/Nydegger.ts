9baeb927cc55a4231cef4817761bbc4948c4ca9f
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-08-01 12:19:31 -0500

    Fix Nydegger strategy scoreMap

--- src/strategies/Nydegger.ts
+++ src/strategies/Nydegger.ts
@@ -6,15 +6,15 @@ class Nydegger extends StrategyWithHistory {
     1, 6, 7, 17, 22, 23, 26, 29, 30, 31, 33, 38, 39, 45, 49, 54, 55, 58, 61,
   ];
 
-  private scoreMap = new Map<[boolean, boolean], number>([
-    [[true, true], 0],
-    [[true, false], 2],
-    [[false, true], 1],
-    [[false, false], 3],
+  private scoreMap = new Map<string, number>([
+    ["true,true", 0],
+    ["true,false", 2],
+    ["false,true", 1],
+    ["false,false", 3],
   ]);
 
   private getScorePerMove(ownMove: Move, opponentMove: Move): number {
-    return this.scoreMap.get([ownMove, opponentMove])!;
+    return this.scoreMap.get(`${ownMove},${opponentMove}`)!;
   }
 
   private getScore(): number {
