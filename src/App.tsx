c958f0682337f47e101148a0c197b3d76097cdd9
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-08-02 22:22:25 -0500

    Add MantineProvider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import SimulationGame from "./components/SimulationGame";
+import { MantineProvider } from "@mantine/core";
 
 function App() {
   return (
-    <>
+    <MantineProvider>
       <p>Hello World</p>
       <SimulationGame />
-    </>
+    </MantineProvider>
   );
 }
 
