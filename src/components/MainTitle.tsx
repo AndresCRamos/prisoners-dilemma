29ad3a24becc2d6b20c46c142463f1459c1ccfa7
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-08-12 00:07:36 -0500

    Add responsive column behavior on Main Title component

--- src/components/MainTitle.tsx
+++ src/components/MainTitle.tsx
@@ -1,15 +1,18 @@
-import { Grid, Title } from "@mantine/core";
+import { Grid, Text, Title } from "@mantine/core";
 
 function MainTitle() {
   return (
     <>
-      <Grid columns={2}>
-        <Title
-          className="text-center my-4 bg-gradient-to-t from-[--mantine-primary-color-filled] inline-block text-transparent bg-clip-text"
-          variant="gradient"
-        >
-          The Prisoners Dilemma
-        </Title>
+      <Grid align="center" justify="center">
+        <Grid.Col span={{ base: 12, sm: 6 }}>
+          <Title className="text-center my-4 bg-gradient-to-t from-[--mantine-primary-color-filled] inline-block text-transparent bg-clip-text border-white border-2 focus:border-red-600">
+            The Prisoners Dilemma
+          </Title>
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, sm: 6 }}>
+          <Text>The most famous game theory paradox</Text>
+          <Text> Test</Text>
+        </Grid.Col>
       </Grid>
     </>
   );
