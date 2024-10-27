9f1d9d46b872dadf23c2d20b1a45b846f6eb83cb
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-10-26 23:43:07 -0500

    Simplify Section component

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,11 +1,13 @@
-import { Container } from "@mantine/core";
+import { Stack } from "@mantine/core";
 import React from "react";
 import classes from "./Section.module.css";
 
 const Section: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <section className={classes.Section}>
-      <Container>{children}</Container>
+    <section>
+      <Stack justify="center" className={classes.Section}>
+        {children}
+      </Stack>
     </section>
   );
 };
