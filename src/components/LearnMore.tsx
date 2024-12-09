e35241e24a5cc016cad5501dc0d1b221867fe84f
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-12-08 20:30:47 -0500

    Analizing the situation

--- src/components/LearnMore.tsx
+++ src/components/LearnMore.tsx
@@ -1,4 +1,4 @@
-import { Blockquote, Container, Stack, Text, Title } from "@mantine/core";
+import { Blockquote, Container, List, Stack, Text, Title } from "@mantine/core";
 import React from "react";
 const LearnMore: React.FC = () => {
   return (
@@ -32,6 +32,33 @@ const LearnMore: React.FC = () => {
             lesser charge will be dropped, getting you free, but your friend
             would spend 5 years in prison for the main charge
           </Text>
+          <Title order={4}>The catch?</Title>
+          <Text>
+            You know is too good to be true, so you ask the cop about the catch,
+            and he tells you that your friend is given the same offer, if he
+            betrays you, he will go free, and you will be in jail for 5 years
+          </Text>
+          <Text>
+            He tells you as well, that if you both betray each other, both of
+            you get a sentence of 3 years
+          </Text>
+          <Title order={4}>What would you do?</Title>
+          <Text>
+            The cop does not let you think about this much, remember,your friend
+            has the same options as you, so you start to weight your options:
+          </Text>
+          <List withPadding listStyleType="initial">
+            <List.Item>If both remain silent, both get 1 year</List.Item>
+            <List.Item>
+              If you betray your friend, you get free, but he gets 5 years (and
+              he gets mad at you)
+            </List.Item>
+            <List.Item>
+              If he betrays you, he gets free, but you get 5 years (and he gets
+              mad at you)
+            </List.Item>
+            <List.Item>If both betray each other, both get 3 year</List.Item>
+          </List>
         </Stack>
       </Container>
     </>
