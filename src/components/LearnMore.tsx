7d104e1346242b6e0c0e5a21ca695d62a5d77b03
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-12-08 20:27:50 -0500

    Intro to explanation

--- src/components/LearnMore.tsx
+++ src/components/LearnMore.tsx
@@ -1,10 +1,23 @@
-import { Container, Stack } from "@mantine/core";
+import { Blockquote, Container, Stack, Text, Title } from "@mantine/core";
 import React from "react";
 const LearnMore: React.FC = () => {
   return (
     <>
       <Container>
-        <Stack></Stack>
+        <Stack>
+          <Title order={2}>What's the Prisoners Dilemma?</Title>
+          <Text>The prisoners dilemma in its formal form, is defined as:</Text>
+          <Blockquote>
+            Is a game theory thought experiment involving two rational agents,
+            each of whom can either cooperate for mutual benefit or betray their
+            partner ("defect") for individual gain
+          </Blockquote>
+          <Text>A normal game follows the same structure</Text>
+          <Text>
+            But a more easier to get explanation would be actually explaining
+            the game
+          </Text>
+        </Stack>
       </Container>
     </>
   );
