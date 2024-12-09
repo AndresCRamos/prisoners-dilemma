c6c73ded99fa43ea3ae364ef8485de4be7d27648
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-12-08 20:28:53 -0500

    Basic explanation of prisoners Dilemma

--- src/components/LearnMore.tsx
+++ src/components/LearnMore.tsx
@@ -17,6 +17,21 @@ const LearnMore: React.FC = () => {
             But a more easier to get explanation would be actually explaining
             the game
           </Text>
+          <Title order={3}>Really what's the Prisoners Dilemma?</Title>
+          <Text>
+            Imagine this, you and a friend are arrested for being suspects for a
+            crime, you are not allow to communicate whatsoever with your friend,
+            and the cop tells you that, while they have evidence for a lesser
+            charge, that will get you and your friend each one year in jail, he
+            admits they dont have enough evidence for the main charge, so he
+            offers a bargain.
+          </Text>
+          <Title order={4}>Bargain: Betray your friend?</Title>
+          <Text>
+            If you decide to testify against your friend, betraying him, your
+            lesser charge will be dropped, getting you free, but your friend
+            would spend 5 years in prison for the main charge
+          </Text>
         </Stack>
       </Container>
     </>
