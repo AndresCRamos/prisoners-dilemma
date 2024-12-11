79f2d35ff60fa821bbd2727260a990be471911f3
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-12-10 20:29:44 -0500

    Back to basics

--- src/components/LearnMore.tsx
+++ src/components/LearnMore.tsx
@@ -67,6 +67,43 @@ const LearnMore: React.FC = () => {
             friend decide to betray each other, both will end with 3 years, when
             there is a better outcome, remain silent and get 1 year each
           </Text>
+          <Title order={2}>Back to basics: The formal side</Title>
+          <Text>
+            Now that we have an intuitive understanding of the game itself, lets
+            define it in more formal game theory terms:
+          </Text>
+          <List listStyleType="initial" withPadding>
+            <List.Item>
+              <span className="font-bold">Agent</span> refers to each of the
+              players involved, that means your friend and you
+            </List.Item>
+            <List.Item>
+              <span className="font-bold">Cooperate</span> means remain silent
+            </List.Item>
+            <List.Item>
+              {" "}
+              <span className="font-bold">Defect</span> means betray your friend
+            </List.Item>
+          </List>
+          <Text>The outcomes for each one turn out to be:</Text>
+          TODO: Create table of outcomes
+          <Text>In this form, the outcomes can be described as:</Text>
+          <List listStyleType="initial" withPadding>
+            <List.Item>
+              <span className="font-bold">
+                Mutual cooperation (both silent)
+              </span>{" "}
+              is the best scenario for both, but is risky
+            </List.Item>
+            <List.Item>
+              <span className="font-bold">Mutual defection (both betray)</span>{" "}
+              is the most stable scenario, as neither can do better without risk
+            </List.Item>
+            <List.Item>
+              <span className="font-bold">Cooperation over defection</span>{" "}
+              benefits the defector and hurts the cooperator
+            </List.Item>
+          </List>
           <Title order={2}>Why its important?</Title>
           <Text>
             This shows the real dilemma: the choice between looking out for
