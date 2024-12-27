618948f655cc2cc49d7cadcbcc5664ea73d12dc2
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-12-26 22:36:26 -0500

    Add Tournament rules

--- src/components/LearnMore/AxelrodTournament.tsx
+++ src/components/LearnMore/AxelrodTournament.tsx
@@ -1,5 +1,6 @@
 import { Stack, Text, Title } from "@mantine/core";
 import React from "react";
+import AxelrodOutcomeTable from "./AxelrodOutcomeTable";
 
 const AxelrodTournament: React.FC = () => {
   return (
@@ -17,6 +18,30 @@ const AxelrodTournament: React.FC = () => {
         gets in the way. To understand how cooperation could thrive in a world
         of selfish individuals, Axelrod designed a tournament where participants
         submitted strategies to play the Prisoner’s Dilemma against each other.
+      </Text>
+      <Title order={3} className="text-[--mantine-primary-color-filled]">
+        The Tournament
+      </Title>
+      <Text>
+        To adapt the dilemma o its new context, some changes were necessary, now
+        instead of minimizing a prison sentence, the game was about maximizing
+        points, so the payoffs where inverted
+      </Text>
+      <AxelrodOutcomeTable />
+      <Text>
+        Since real-life interactions aren’t one-and-done (especially for social
+        beings like humans), Axelrod made the tournament iterative, with each
+        strategy playing multiple rounds against others.
+      </Text>
+      <Title order={4} className="text-[--mantine-primary-color-filled]">
+        The Rules
+      </Title>
+      <Text>
+        Axelrod established a few ground rules: Each strategy played against
+        every other strategy, itself, and a random strategy. Each match
+        consisted of 200 turns. To ensure reliability, the entire tournament was
+        repeated five times. With the rules set and strategies submitted, the
+        tournament began—and the results were anything but predictable.
       </Text>
     </Stack>
   );
