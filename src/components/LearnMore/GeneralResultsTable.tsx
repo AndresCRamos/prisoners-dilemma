6a8b6ca61129a64e7f55bc24f772d34e8a80038c
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2025-01-10 18:04:54 -0500

    Create basic skeleton for General Table Results

--- src/components/LearnMore/GeneralResultsTable.tsx
+++ src/components/LearnMore/GeneralResultsTable.tsx
@@ -1,7 +1,23 @@
+import { Table } from "@mantine/core";
 import React from "react";
+import { rows } from "./data";
 
 const GeneralResultsTable: React.FC = () => {
-  return <></>;
+  return (
+    <Table.ScrollContainer minWidth={320}>
+      <Table>
+        <Table.Tbody>
+          {rows.map((row, rowIndex) => (
+            <Table.Tr tabIndex={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <Table.Td tabIndex={cellIndex}>{cell}</Table.Td>
+              ))}
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
+    </Table.ScrollContainer>
+  );
 };
 
 export default GeneralResultsTable;
