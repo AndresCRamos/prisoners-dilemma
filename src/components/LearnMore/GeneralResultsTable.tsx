6e83dd247694d04c4a447e17909d6c5c2b1dfce2
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2025-01-10 19:11:33 -0500

    Add accordion component to hide GeneralResultsTable

--- src/components/LearnMore/GeneralResultsTable.tsx
+++ src/components/LearnMore/GeneralResultsTable.tsx
@@ -1,35 +1,42 @@
-import { Table } from "@mantine/core";
+import { Accordion, Table } from "@mantine/core";
 import React from "react";
 import { rows } from "./data";
 
 const GeneralResultsTable: React.FC = () => {
   return (
-    <Table.ScrollContainer minWidth={320}>
-      <Table
-        withColumnBorders
-        striped
-        layout="fixed"
-        className="w-full border-collapse "
-      >
-        <Table.Tbody>
-          {rows.map((row, rowIndex) => (
-            <Table.Tr
-              tabIndex={rowIndex}
-              className="first:font-bold first:bg-[--mantine-primary-color-filled]"
+    <Accordion>
+      <Accordion.Item value="See results">
+        <Accordion.Control>See results</Accordion.Control>
+        <Accordion.Panel>
+          <Table.ScrollContainer minWidth={320}>
+            <Table
+              withColumnBorders
+              striped
+              layout="fixed"
+              className="w-full border-collapse "
             >
-              {row.map((cell, cellIndex) => (
-                <Table.Td
-                  tabIndex={cellIndex}
-                  className="text-center first:w-[22ch] w-[12ch] first:font-bold first:bg-[--mantine-primary-color-filled]"
-                >
-                  {cell}
-                </Table.Td>
-              ))}
-            </Table.Tr>
-          ))}
-        </Table.Tbody>
-      </Table>
-    </Table.ScrollContainer>
+              <Table.Tbody>
+                {rows.map((row, rowIndex) => (
+                  <Table.Tr
+                    tabIndex={rowIndex}
+                    className="first:font-bold first:bg-[--mantine-primary-color-filled]"
+                  >
+                    {row.map((cell, cellIndex) => (
+                      <Table.Td
+                        tabIndex={cellIndex}
+                        className="text-center first:w-[22ch] w-[12ch] first:font-bold first:bg-[--mantine-primary-color-filled]"
+                      >
+                        {cell}
+                      </Table.Td>
+                    ))}
+                  </Table.Tr>
+                ))}
+              </Table.Tbody>
+            </Table>
+          </Table.ScrollContainer>
+        </Accordion.Panel>
+      </Accordion.Item>
+    </Accordion>
   );
 };
 
