79dee3cd7c22a7f74a37ad29777ec1519ded0ec9
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2025-02-18 22:36:00 -0500

    Create list of important resources

--- src/components/Sources/Sources.tsx
+++ src/components/Sources/Sources.tsx
@@ -1,7 +1,22 @@
+import { Container, List, Stack, Text, Title } from "@mantine/core";
 import React from "react";
 
 const Sources: React.FC = () => {
-  return <></>;
+  // TODO: Create description for resources
+  return (
+    <Container>
+      <Stack gap={"lg"}>
+        <Title className="text-[--mantine-primary-color-filled]">Sources</Title>
+        <Text>This project draws inspiration from some key resources:</Text>
+
+        <List listStyleType="initial">
+          <List.Item>Veritasium Video</List.Item>
+          <List.Item>Prisoners Dilemma paper</List.Item>
+          <List.Item>Axelrod library</List.Item>
+        </List>
+      </Stack>
+    </Container>
+  );
 };
 
 export default Sources;
