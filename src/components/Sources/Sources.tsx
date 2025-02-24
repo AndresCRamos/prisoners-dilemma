8d4bb5e6689432588f20c967f5dbbc3427a0698f
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2025-02-23 22:29:59 -0500

    Add card title styles

--- src/components/Sources/Sources.tsx
+++ src/components/Sources/Sources.tsx
@@ -9,29 +9,34 @@ const Sources: React.FC = () => {
           Sources
         </Title>
         <Text>This project draws inspiration from some key resources:</Text>
+
         <Card shadow="md" padding="xl" radius={"md"} withBorder>
-          <Stack>
-            <Title
-              order={3}
-              className="text-[--mantine-primary-color-filled] text-center"
-            >
-              What Game Theory Reveals About Life, The Universe, and Everything
-            </Title>
-            <Text className="text-center">[Veritasium - Youtube]</Text>
-            <Text>
-              The main spark for this project, an introduction to the Prisoners
-              Dilemma made by Veritasium, a personal favorite science channel.
-              It goes in more depth about the prisoners dilemma, Axelrod
-              tournament, some variations on it, and some insights about
-              Axelrod's discoveries
-            </Text>
-          </Stack>
+          <Title
+            order={3}
+            className="text-[--mantine-primary-color-filled] text-center"
+          >
+            What Game Theory Reveals About Life, The Universe, and Everything
+          </Title>
+          <Text className="text-center font-semibold pb-4" c="dimmed">
+            [Veritasium - Youtube]
+          </Text>
+          <Text>
+            The main spark for this project, an introduction to the Prisoners
+            Dilemma made by Veritasium, a personal favorite science channel. It
+            goes in more depth about the prisoners dilemma, Axelrod tournament,
+            some variations on it, and some insights about Axelrod's discoveries
+          </Text>
         </Card>
         <Card shadow="md" padding="xl" radius={"md"} withBorder>
-          <Title order={3} className="text-[--mantine-primary-color-filled]">
+          <Title
+            order={3}
+            className="text-[--mantine-primary-color-filled] text-center"
+          >
             Effective choice in the Prisoners Dilemma
           </Title>
-          <Text>[Axelrod - 1980]</Text>
+          <Text className="text-center font-semibold pb-4" c="dimmed">
+            [Axelrod - 1980]
+          </Text>
           <Text>
             The original source, Axelrod wrote this paper from a tournament he
             held, where several strategies submitted by some colleges competed
@@ -40,10 +45,15 @@ const Sources: React.FC = () => {
           </Text>
         </Card>
         <Card shadow="md" padding="xl" radius={"md"} withBorder>
-          <Title order={3} className="text-[--mantine-primary-color-filled]">
+          <Title
+            order={3}
+            className="text-[--mantine-primary-color-filled] text-center"
+          >
             Axelrod library
           </Title>
-          <Text>[Python pip: Axelrod]</Text>
+          <Text className="text-center font-semibold pb-4" c="dimmed">
+            [Python pip: Axelrod]
+          </Text>
           <Text>
             Most of the implementation details of the strategies themselves come
             from a refactor of this great Python lib, as well as some ideas and
